1ca9bae75509d6407b7cef3bf42f95e79cca33c1
fke04ju <justin881110@gmail.com>
2019-07-05 15:21:41 +0800

    7/5

--- Exam/Exam_2018/50139/GPA_calculation.c
+++ Exam/Exam_2018/50139/GPA_calculation.c
@@ -13,5 +13,55 @@ struct Student{
 };
  
 void GPA_calculation(struct Student all_student[], int N){
-	
+	for(int i = 0;i<N;i++){
+		for(int j = 0;j<all_student[i].N_class;j++){
+			if(all_student[i].all_class[j].score[0] == 'A'){
+				if(all_student[i].all_class[j].score[1] == '+'){
+					all_student[i].GPA += (all_student[i].all_class[j].academic_credit*4.3);
+				}else if(all_student[i].all_class[j].score[1] == '-'){
+					all_student[i].GPA += (all_student[i].all_class[j].academic_credit*3.7);
+				}else{
+					all_student[i].GPA += (all_student[i].all_class[j].academic_credit*4);
+				}
+			}else if(all_student[i].all_class[j].score[0] == 'B'){
+				if(all_student[i].all_class[j].score[1] == '+'){
+					all_student[i].GPA += (all_student[i].all_class[j].academic_credit*3.3);
+				}else if(all_student[i].all_class[j].score[1] == '-'){
+					all_student[i].GPA += (all_student[i].all_class[j].academic_credit*2.7);
+				}else{
+					all_student[i].GPA += (all_student[i].all_class[j].academic_credit*3.0);
+				}
+			}else if(all_student[i].all_class[j].score[0] == 'C'){
+				if(all_student[i].all_class[j].score[1] == '+'){
+					all_student[i].GPA += (all_student[i].all_class[j].academic_credit*2.3);
+				}else if(all_student[i].all_class[j].score[1] == '-'){
+					all_student[i].GPA += (all_student[i].all_class[j].academic_credit*1.7);
+				}else{
+					all_student[i].GPA += (all_student[i].all_class[j].academic_credit*2.0);
+				}
+			}else{
+				all_student[i].GPA += 0;
+			}
+			all_student[i].N_credit += all_student[i].all_class[j].academic_credit;
+		}
+		all_student[i].GPA /= all_student[i].N_credit;
+	}
+	for(int i = 0;i<N-1;i++){
+		for(int j = 0;j<N-i-1;j++){
+			if(all_student[j].GPA < all_student[j+1].GPA){
+				struct Student tmp;
+				tmp = all_student[j];
+				all_student[j] = all_student[j+1];
+				all_student[j+1] = tmp;
+			}
+		}
+	}
+	int award_num = ((N-1)/20)+1;
+	int award_count = 1;
+	for(int i = 0;i<N && award_count <= award_num;i++){
+		if(all_student[i].GPA >= 3.38 && all_student[i].N_credit >= 15){
+			printf("%d %s %f\n",award_count,all_student[i].name,all_student[i].GPA);
+		}
+		award_count++;
+	}
 }
