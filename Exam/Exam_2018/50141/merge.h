bf3836178e421524a330148b32b77abc0a7d4158
yvecca <justin881110@gmail.com>
2019-09-11 01:00:16 +0800

    update

--- Exam/Exam_2018/50141/merge.h
+++
@@ -1,8 +0,0 @@
-#ifndef MERGE_H
-#define MERGE_H
-typedef struct node {
-    int data;
-    struct node *next;
-}Node;
-Node *merge(Node *list[], int K);
-#endif
--- Exam/Exam_2018/50145/sub.h
+++
@@ -1,7 +0,0 @@
-typedef struct Node
-{
-    char c;
-    struct Node *next;
-}Node;
-void substring(Node* text, Node* pattern);
-void subsequence(Node* text, Node* pattern);
