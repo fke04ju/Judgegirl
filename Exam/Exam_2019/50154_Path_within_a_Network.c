f862ea95d22e4161ffcd05d67a782d5c67a02114
yvecca <justin881110@gmail.com>
2019-09-10 21:57:41 +0800

    9/10

--- Exam/Exam_2019/50154_Path_within_a_Network.c
+++ Exam/Exam_2019/50154_Path_within_a_Network.c
@@ -3,10 +3,7 @@
 int main(){
     int a,b,c,d,e,f,g;
     scanf("%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g);
-    int as = a,bs = b,cs = c,ds = d,es = 0,fs = 0,gs = 0;
-    es = (a+b+c)*e;
-    fs = (c+d)*f;
-    gs = e*g;
+    int as = a,bs = b,cs = c,ds = d,es = (a+b+c)*e,fs = (c+d)*f,gs = e*g;
     int end = g+f;
     int edge = as+bs+cs+ds+es+fs+gs+end;
     int path = a*e*g+b*e*g+c*e*g+c*f+d*f;
